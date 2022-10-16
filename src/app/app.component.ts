c07e88a5b8937021fcf0d80914449b09e2406cbe
Thomas <thomasbraendle94@gmail.com>
2022-10-16 18:03:06 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'V_1.0.0-0002_16.10.2022';
+}
