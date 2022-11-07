c420bd444942ded6c932e1fbff95138808276281
Thomas <thomasbraendle94@gmail.com>
2022-11-07 21:53:42 +0100

    07.11.2022

--- src/app/_hauptKomponente/l-einstellungen/l-einstellungen.component.ts
+++ src/app/_hauptKomponente/l-einstellungen/l-einstellungen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { GruppenComponent } from 'src/app/_popupKomponente/gruppen/gruppen.component';
 
 @Component({
   selector: 'app-l-einstellungen',
@@ -7,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LEinstellungenComponent implements OnInit {
 
-  constructor() { }
+  constructor(public matDialog: MatDialog) { }
+
+  openGruppen() {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.id = "modal-component";
+    dialogConfig.height = "510px";
+    dialogConfig.width = "894px";
+
+    const modalDialog = this.matDialog.open(GruppenComponent, dialogConfig);
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/_popupKomponente/gruppen/gruppen.component.ts
+++ src/app/_popupKomponente/gruppen/gruppen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-gruppen',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gruppen.component.sass']
 })
 export class GruppenComponent implements OnInit {
+  close:string = 'assets/img/icon/close.png';
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<GruppenComponent>) { }
 
+  closeModal() {
+    this.dialogRef.close();
+  }
+  
   ngOnInit(): void {
   }
 
